66c06e45db8a19dbf4049791e8af787d831dca02
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2024-02-21 06:41:31 +0100

    Create/update EmployerService tests.

--- src/test/kotlin/krystian/kryszczak/recruitment/service/account/employer/EmployerServiceTest.kt
+++ src/test/kotlin/krystian/kryszczak/recruitment/service/account/employer/EmployerServiceTest.kt
@@ -1,4 +1,41 @@
 package krystian.kryszczak.recruitment.service.account.employer
 
-class EmployerServiceTest {
-}
+import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
+import krystian.kryszczak.recruitment.model.account.employer.EmployerAccount
+import krystian.kryszczak.recruitment.service.account.AccountServiceTest
+
+@MicronautTest(transactional = false)
+class EmployerServiceTest(accountService: EmployerAccountService) : AccountServiceTest<EmployerAccount>(
+accountService,
+arrayOf(
+    EmployerAccount(
+        null,
+        "fsociety",
+        "we are fsociety | we are finally free | we are finally awake ;^)",
+        "Other",
+        "Information services",
+        3,
+        "https://linkedin.com/company/fs0ciety",
+        null,
+        null,
+        "https://linkedin.com/company/fs0ciety",
+        null,
+        null,
+        null
+    ), EmployerAccount(
+        null,
+        "Evil Corp LLC",
+        "E Corp is the leading global provider of corporate strategy, philanthropy, sustainability, and growth.",
+        "Corporation",
+        "HTB{WW91IGNhbiBkbyB0aGlzLCBrZWVwIGdvaW5nISEh}",
+        1000000,
+        "https://www.e-corp-usa.com/",
+        null,
+        null,
+        "https://www.linkedin.com/company/evil-corp-llc/",
+        null,
+        null,
+        null
+    ),
+), { it.copy(name = "${it.name} - ${it.name}") },
+{ item, id -> item.copy(id = id) })
