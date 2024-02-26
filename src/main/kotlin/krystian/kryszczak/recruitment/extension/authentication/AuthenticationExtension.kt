599be5dd83fcebfc3a456b2a12d596554868fc26
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2024-02-26 12:48:51 +0100

    Create authentication extension.

--- src/main/kotlin/krystian/kryszczak/recruitment/extension/authentication/AuthenticationExtension.kt
+++ src/main/kotlin/krystian/kryszczak/recruitment/extension/authentication/AuthenticationExtension.kt
@@ -1,9 +1,6 @@
 package krystian.kryszczak.recruitment.extension.authentication
 
 import io.micronaut.security.authentication.Authentication
-import krystian.kryszczak.recruitment.constatnt.role.UserRole
 
-fun Authentication.getClientUserRole() = attributes["ROLE"]?.toString()?.let { UserRole.parse(it) }
+fun Authentication.getClientUserRole() = attributes["ROLE"]?.toString()?.uppercase()
 fun Authentication.getClientId() = attributes["ID"]?.toString()
-fun Authentication.isClientCandidate() = getClientUserRole() == UserRole.CANDIDATE
-fun Authentication.isClientEmployer() = getClientUserRole() == UserRole.EMPLOYER
