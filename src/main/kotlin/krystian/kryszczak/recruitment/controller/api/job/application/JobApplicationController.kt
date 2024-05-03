9184011e2c623eacba94bb9cb4dd16ab1d9a8de1
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2024-05-03 21:01:49 +0200

    Update job application controller.

--- src/main/kotlin/krystian/kryszczak/recruitment/controller/api/job/application/JobApplicationController.kt
+++ src/main/kotlin/krystian/kryszczak/recruitment/controller/api/job/application/JobApplicationController.kt
@@ -1,9 +1,11 @@
 package krystian.kryszczak.recruitment.controller.api.job.application
 
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Delete
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.Status
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
 import io.micronaut.security.authentication.Authentication
@@ -13,26 +15,25 @@ import krystian.kryszczak.recruitment.service.job.application.JobApplicationServ
 
 @Controller("api/v1/job/applications/")
 @ExecuteOn(TaskExecutors.BLOCKING)
-class JobApplicationController(private val service: JobApplicationService) { // TODO get candidate applications and cancel, pageination
-    @RolesAllowed("EMPLOYER")
-    @Get("offer/{id:$ID_PATTERN}")
-    fun getForOffer(@PathVariable id: String, authentication: Authentication) = service.findByEmployerClient(id, authentication)
-
+class JobApplicationController(private val service: JobApplicationService) {
     @RolesAllowed("EMPLOYER")
     @Get("/{id:$ID_PATTERN}")
-    fun get(@PathVariable id: String, authentication: Authentication) = service.findByEmployerClient(id, authentication)
+    fun get(@PathVariable id: String, authentication: Authentication) =
+        service.findByIdForEmployerClient(id, authentication)
+
+    @RolesAllowed("EMPLOYER")
+    @Get("offer/{id:$ID_PATTERN}")
+    fun getByOfferId(@PathVariable id: String, authentication: Authentication) =
+        service.findByOfferIdForEmployerClient(id, authentication)
 
     @RolesAllowed("CANDIDATE")
     @Get("/own{/page}")
-    fun getOwn(page: Int?, authentication: Authentication) = service.findSentByCandidateClient(authentication)
+    fun findOwn(@PathVariable(defaultValue = "0") page: Int, authentication: Authentication) =
+        service.findPublishedByCandidateClient(authentication)
 
+    @Status(HttpStatus.ACCEPTED)
     @RolesAllowed("CANDIDATE")
-    @Get("/own/{id:$ID_PATTERN}")
-    fun findOwn(@PathVariable id: String, authentication: Authentication) =
-        service.findByIdSentByCandidateClient(id, authentication)
-
-    @RolesAllowed("CANDIDATE")
-    @Delete("/own/{id:$ID_PATTERN}")
-    fun cancelOwn(@PathVariable id: String, authentication: Authentication) =
-        service.cancelOwnById(id, authentication)
+    @Delete("/{id:$ID_PATTERN}")
+    fun delete(@PathVariable id: String, authentication: Authentication) =
+        service.deleteOwnByIdForCandidateClient(id, authentication)
 }
